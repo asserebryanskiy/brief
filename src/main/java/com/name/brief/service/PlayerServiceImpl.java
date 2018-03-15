c4d44749d322879821d0733788c81d06a954e504
asserebryanskiy <asserebryanskiy@gmail.com>
2018-03-15 18:19:29 +0300

    added setSessionRegistry(), findByUsername(), changed logout() logic

--- src/main/java/com/name/brief/service/PlayerServiceImpl.java
+++ src/main/java/com/name/brief/service/PlayerServiceImpl.java
@@ -3,13 +3,20 @@ package com.name.brief.service;
 import com.name.brief.model.Player;
 import com.name.brief.repository.PlayerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.session.SessionInformation;
+import org.springframework.security.core.session.SessionRegistry;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.security.Principal;
+
 @Service
 public class PlayerServiceImpl implements PlayerService {
+
     private final PlayerRepository playerRepository;
+    private SessionRegistry sessionRegistry;
 
     @Autowired
     public PlayerServiceImpl(PlayerRepository playerRepository) {
@@ -23,7 +30,8 @@ public class PlayerServiceImpl implements PlayerService {
         return player;
     }
 
-    private Player findByUsername(String username) {
+    @Override
+    public Player findByUsername(String username) {
         return playerRepository.findByUsername(username);
     }
 
@@ -33,13 +41,15 @@ public class PlayerServiceImpl implements PlayerService {
     }
 
     @Override
-    public void login(Player player) {
+    public void setLoggedIn(Player player) {
         player.setLoggedIn(true);
         playerRepository.save(player);
     }
 
     @Override
     public void logout(Player player) {
+        sessionRegistry.getAllSessions(player, false)
+                .forEach(SessionInformation::expireNow);
         player.setLoggedIn(false);
         playerRepository.save(player);
     }
@@ -48,5 +58,16 @@ public class PlayerServiceImpl implements PlayerService {
     public void addResponses(Player player, String responses, int roundIndex) {
         player.getDecision(roundIndex).setAnswer(responses);
         playerRepository.save(player);
+    }
+
+    @Override
+    public void setSessionRegistry(SessionRegistry sessionRegistry) {
+        this.sessionRegistry = sessionRegistry;
+    }
+
+    @Override
+    public boolean isLoggedIn(Player player) {
+        return sessionRegistry != null &&
+                sessionRegistry.getAllSessions(player, false).size() > 0;
     }
 }
