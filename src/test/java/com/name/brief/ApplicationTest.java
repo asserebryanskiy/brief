7ecc89a96ec59933b669a74ec94d291cdb438d48
asserebryanskiy <asserebryanskiy@gmail.com>
2018-03-29 20:12:35 +0300

    prepared for heroku posgresql database

--- src/test/java/com/name/brief/ApplicationTest.java
+++ src/test/java/com/name/brief/ApplicationTest.java
@@ -9,7 +9,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@TestPropertySource("classpath:application.properties")
 public class ApplicationTest {
     @Test
     public void contextLoads() {
