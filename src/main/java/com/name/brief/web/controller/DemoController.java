3d1c45fe76433adff1a0639fa0b0b12c253b705e
asserebryanskiy <asserebryanskiy@gmail.com>
2018-04-10 10:22:37 +0300

    hotfix on risk map demo desighn

--- src/main/java/com/name/brief/web/controller/DemoController.java
+++ src/main/java/com/name/brief/web/controller/DemoController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class DemoController {
-    @RequestMapping("/demo/riskMap")
+    @RequestMapping({"/demo/riskMap", "/demo"})
     public String getRiskMapDemo(Model model) {
         model.addAttribute("game", new RiskMap());
         model.addAttribute("decision", null);
