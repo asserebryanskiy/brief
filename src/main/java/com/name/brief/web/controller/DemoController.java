2f5f23655f44b945d99dc5494fa7a62a5f902532
asserebryanskiy <asserebryanskiy@gmail.com>
2018-04-16 16:21:33 +0300

    changed timer to 3 minutes, replaced elements in riskMapDemo to th:replace

--- src/main/java/com/name/brief/web/controller/DemoController.java
+++ src/main/java/com/name/brief/web/controller/DemoController.java
@@ -1,5 +1,6 @@
 package com.name.brief.web.controller;
 
+import com.name.brief.model.GameSession;
 import com.name.brief.model.games.RiskMap;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,7 +10,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class DemoController {
     @RequestMapping({"/E86CD2557B8F9C99"})
     public String getRiskMapDemo(Model model) {
-        model.addAttribute("game", new RiskMap());
+        model.addAttribute("gameSession", new GameSession.GameSessionBuilder("id")
+                .withGame(new RiskMap()).build());
         model.addAttribute("decision", null);
         model.addAttribute("demo", true);
         return "game/riskMapDemo";
