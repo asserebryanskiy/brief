8bcebf4f822f1afb8fa08815a1a96da79f553281
asserebryanskiy <asserebryanskiy@gmail.com>
2018-05-06 21:32:37 +0300

    added two last phases

--- src/main/java/com/name/brief/DevDatabaseLoader.java
+++ src/main/java/com/name/brief/DevDatabaseLoader.java
@@ -91,7 +91,7 @@ public class DevDatabaseLoader implements ApplicationRunner {
                 .withGame(game)
                 .withUser(moderator1)
                 .build();
-        game.setPhaseIndex(10);
+        game.setPhaseIndex(12);
         gameSessionService.save(session);
         gameSessionService.save(session2);
         gameSessionService.save(session3);
