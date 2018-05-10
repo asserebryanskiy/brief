ea4a3ef20def65ac1f65a337cd18ef4360efd743
asserebryanskiy <asserebryanskiy@gmail.com>
2018-05-10 13:24:22 +0300

    cleared dev db loader

--- src/main/java/com/name/brief/DevDatabaseLoader.java
+++ src/main/java/com/name/brief/DevDatabaseLoader.java
@@ -42,46 +42,7 @@ public class DevDatabaseLoader implements ApplicationRunner {
         List<User> users = new ArrayList<>(2);
         String password = SecurityConfig.passwordEncoder.encode("password");
         User moderator1 = new User("moderator1", password, Role.MODERATOR.getRole());
-        User moderator2 = new User("moderator2", password, Role.MODERATOR.getRole());
         users.add(moderator1);
-        users.add(moderator2);
-        users.add(new User("admin", password, Role.ADMIN.getRole()));
         userRepository.save(users);
-
-        GameSession session = new GameSession.GameSessionBuilder("brief")
-                .withNumberOfCommands(5)
-                .withUser(moderator1)
-                .build();
-
-        session.getPlayers().forEach(p -> {
-            String[] correctAnswers = (String[]) session.getGame().getCorrectAnswers();
-            String[] additions = {"", "A2", "A2A4", "A2A4B4", "A2A4B4C4"};
-            for (int i = 0; i < p.getDecisions().size(); i++) {
-                p.getDecisions().get(i).setAnswer(
-                        correctAnswers[i] + additions[new Random().nextInt(additions.length)]);
-            }
-        });
-
-        GameSession session2 = new GameSession.GameSessionBuilder("testtest")
-                .withNumberOfCommands(3)
-                .withUser(moderator1)
-                .build();
-        GameSession session3 = new GameSession.GameSessionBuilder("otherUserSession")
-                .withUser(moderator2)
-                .build();
-        GameSession pastSession = new GameSession.GameSessionBuilder("otherUserSession")
-                .withActiveDate(LocalDate.now().minusDays(1))
-                .withUser(moderator1)
-                .build();
-        GameSession riskMap = new GameSession.GameSessionBuilder("risk")
-                .withGame(new RiskMap())
-                .withUser(moderator1)
-                .build();
-        ((RiskMap) riskMap.getGame()).setType(RiskMapType.HOTEL);
-        gameSessionService.save(session);
-        gameSessionService.save(session2);
-        gameSessionService.save(session3);
-        gameSessionService.save(pastSession);
-        gameSessionService.save(riskMap);
     }
 }
